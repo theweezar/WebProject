aede50e152527d509c868e17fa46f6adc0e00f52
minhduc <minhducducminh1999@gmail.com>
2019-07-07 22:10:43 +0700

    commited all

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 const request = require('request');
+const fetch = require('node-fetch');
+const Bluebird = require('bluebird');
+fetch.Promise = Bluebird;
 const exphbs  = require('express-handlebars');
 const PORT = process.env.PORT || 8080;
 //https://api.darksky.net/forecast/e08bc4e99f5901c95918de49808b4357/37.8267,-122.4233
@@ -13,26 +16,29 @@ app.use(express.urlencoded({extended:false}));
 app.engine('handlebars', exphbs({defaultLayout:"main"}));
 app.set('view engine', 'handlebars');
 
+// app.get('/',(req,res) => {
+//   const getAPI = new Promise((resolve,reject) => {
+//     request({
+//       method:"GET",
+//       uri:"https://api.darksky.net/forecast/e08bc4e99f5901c95918de49808b4357/37.8267,-122.4233"
+//     },(err,body) => {
+//       if (err) reject(err);
+//       resolve(body);
+//     });
+//   });
+//   getAPI.then(rs => {
+//     res.render('app',{data : JSON.stringify(rs.body)});
+//   }).catch(err => {throw err;});
+// });
+
 app.get('/',(req,res) => {
-  const getAPI = new Promise((resolve,reject) => {
-    request({
-      method:"GET",
-      uri:"https://api.darksky.net/forecast/e08bc4e99f5901c95918de49808b4357/37.8267,-122.4233"
-    },(err,body) => {
-      if (err) reject(err);
-      resolve(body);
-    });
+  let latitude = 10.80089049; // vĩ độ, đường nằm ngang
+  let longitude = 106.70141816; // kinh độ đường nằm dọc
+  fetch(`https://api.darksky.net/forecast/e08bc4e99f5901c95918de49808b4357/${latitude},${longitude}`)
+  .then(rs => rs.json())
+  .then(rs => {
+    res.render('app',{data:JSON.stringify(rs)});
   });
-  getAPI.then(rs => {
-    // console.log(rs.body);
-    const str = JSON.stringify(rs.body);
-    res.render('app',{data : str});
-    // fs.writeFile(path.join(__dirname,'JSON','weather.JSON'),rs.body,(err) => {
-    //   if (err) throw err;
-    //   res.end();
-    // });
-    // res.end();
-  }).catch(err => {throw err;});
 });
 
 app.listen(PORT,() => {
