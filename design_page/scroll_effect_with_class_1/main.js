faa788b5b94e2c63e1cb161f55a2f2ce0b62f392
minhduc <minhducducminh1999@gmail.com>
2019-07-26 18:56:25 +0700

    I find a way to scroll

--- design_page/scroll_effect_with_class_1/main.js
+++ design_page/scroll_effect_with_class_1/main.js
@@ -7,14 +7,24 @@ class ScrollEffect{
     this.current = 0;
   }
   createPage(id = 0){
-    return `<div style="width:100%; height:100%;" class="page" id="page-${id}"> ${this.components[id]} </div>`;
+    return `<div class="page" id="page-${id}"> ${this.components[id]} </div>`;
   }
   render = () => {
     for(var i = 0; i < this.components.length; i++){
       this.parent.innerHTML += this.createPage(i);
     }
     window.addEventListener("mousewheel",(e) => {
-      
+      console.log(e.wheelDelta);
+      if (e.wheelDelta > 0 && this.scroll){ // Go up
+
+      }
+      if (e.wheelDelta < 0 && this.scroll){ // Go down 
+        this.scroll = false;
+        if (this.current < this.components.length - 1){
+          document.getElementById(`page-${++this.current}`).setAttribute("style","height:0;transition:1s;");
+        }
+        // fix bug thẻ page-4 xuất hiện trên cùng trong html
+      }
     });
   }
 }
