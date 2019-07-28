2ac775ce205df909993ec3890ef6f572ded3511f
minhduc <minhducducminh1999@gmail.com>
2019-07-28 16:57:46 +0700

    hey

---
+++ design_page/Resume_CV_2/percent.js
@@ -0,0 +1,34 @@
+class Skill{
+  constructor(parentNodeID="",items = [{name:"",percent:""}]) {
+    this.parent = document.getElementById(parentNodeID);
+    this.items = items;
+  }
+  element = () => {
+    let div = "";
+    this.items.forEach(item => {
+      div+=`<div class="box">
+              <div class="name">${item.name}</div>
+                <div class="percent">
+                  <div style="width:${item.percent}" class="pc-bar"></div>
+                </div>
+              </div>
+            </div>`
+    });
+    return div;
+  }
+  render = () => {
+    this.parent.innerHTML += this.element();
+  }
+}
+
+window.onload = () => {
+  let skill = new Skill("load-percent",[
+    {name:"HTML5",percent:"70%"},
+    {name:"CSS3",percent:"50%"},
+    {name:"Javascript",percent:"60%"},
+    {name:"Nodejs",percent:"30%"},
+    {name:"Java",percent:"35%"},
+    {name:"PHP",percent:"80%"}
+  ]);
+  skill.render();
+}
