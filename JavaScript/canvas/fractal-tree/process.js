6aedde29bbfdd09e6c5b0b66f18afef2bac61603
minhduc <minhducducminh1999@gmail.com>
2019-08-01 22:21:26 +0700

    done

--- JavaScript/canvas/fractal-tree/process.js
+++ JavaScript/canvas/fractal-tree/process.js
@@ -4,21 +4,58 @@ cv.width  = 1000;
 cv.height = 500;
 
 class FractalTree{
-  constructor() {
-    
+  constructor(StartX=0,StartY=0) {
+    this.startPoint = {x:StartX,y:StartY};
+    this.offSetLength = 0.67;
+    this.offSetAngle = 31;
+    this.start();
   }
-  rotate(n=0,l=0,al=0){
-    let dx = Math.sin(n*al) * l;
-    let dy = Math.cos(n*al) * l;
-    // console.log(`x:${this.x} and y:${this.y}`);
-    return {dx:dx,dy:dy};
+  newPoint(current = {x:0,y:0},length=0,angle=0){
+    const angleWithPI = angle * Math.PI / 180;
+    const nPoint = {
+      x: current.x + Math.cos(angleWithPI) * length,
+      y: current.y + Math.sin(angleWithPI) * length 
+    };
+    return nPoint;
   }
-  
+  start(){
+    c.beginPath();
+    c.clearRect(0,0,cv.width,cv.height);
+    c.moveTo(this.startPoint.x,this.startPoint.y);
+    this.drawBranch(this.startPoint,100,90);
+    c.closePath();
+  }
+  drawBranch(point={x:0,y:0},length=0,angle=0){
+    if (length < 4) return;
+
+    c.lineTo(point.x,point.y);
+    c.stroke();
+    let nPoint = this.newPoint(point,length,-angle);
+    this.drawBranch(nPoint,length * this.offSetLength, angle + this.offSetAngle);
+
+    c.moveTo(nPoint.x,nPoint.y);
+    this.drawBranch(nPoint,length * this.offSetLength, angle - this.offSetAngle);
+  }
   
 }
 
-const Tree = new FractalTree();
+const Tree = new FractalTree(cv.width/2,cv.height);
+alert("Press F12, get to the console and type 'animate()' then enter to see animation");
 
-window.addEventListener("click",(e) => {
-  console.log(`mouse_x:${e.x} and mouse_y:${e.y}`);
-})
+// window.addEventListener("mousewheel",e => {
+//   if (e.wheelDelta < 0){
+//     Tree.offSetAngle -= 1;
+//     Tree.start();
+//   }
+//   if (e.wheelDelta > 0){
+//     Tree.offSetAngle += 1;
+//     Tree.start();
+//   }
+// });
+
+function animate(){
+  requestAnimationFrame(animate);
+  Tree.offSetAngle += 1;
+  Tree.start();
+}
+
