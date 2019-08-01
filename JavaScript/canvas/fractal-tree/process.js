718cb95da7ac705d0b934ddfac75c76fbe3f97dc
minhduc <minhducducminh1999@gmail.com>
2019-08-01 09:47:52 +0700

    solve 1 problem

--- JavaScript/canvas/fractal-tree/process.js
+++ JavaScript/canvas/fractal-tree/process.js
@@ -7,28 +7,36 @@ class FractalTree{
   constructor() {
     this.x = cv.width / 2;
     this.y = 400;
-    this.len = 100;
+    this.len = 100; // first length
     this.al = Math.PI / 3;
     this.draw();
   }
-  rotate(n){
-    let dx = Math.sin(n*this.al) * this.len;
-    let dy = Math.cos(n*this.al) * this.len;
-    this.x = this.x + dx;
-    this.y = this.y - dy;
-    console.log(`x:${this.x} and y:${this.y}`);
+  rotate(n,l){
+    let dx = Math.sin(n*this.al) * l;
+    let dy = Math.cos(n*this.al) * l;
+    // this.x = this.x + dx;
+    // this.y = this.y - dy;
+    // console.log(`x:${this.x} and y:${this.y}`);
+    return {dx:dx,dy:dy};
   }
   draw = () =>{
     c.lineWidth = 2;
     c.moveTo(this.x,this.y + this.len);
     c.lineTo(this.x,this.y);
-    this.rotate(1);
-    c.lineTo(this.x,this.y);
-    this.rotate(2);
-    c.lineTo(this.x,this.y);
-    this.rotate(3);
-    c.lineTo(this.x,this.y);
     c.stroke();
+    this.loop(1,this.len);
+  }
+  loop(n,l){
+    // ============= Loop start here ============= //
+    if (l > 4){
+      c.moveTo(this.x,this.y);
+      let dis = this.rotate(n,l);
+      this.x += dis.dx;
+      this.y -= dis.dy;
+      c.lineTo(this.x,this.y);
+      c.stroke();
+      this.loop(++n,l * 0.7);
+    }
   }
 }
 
