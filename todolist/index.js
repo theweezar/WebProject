09dd98890d1b9f64049ff148396ff4e88e912b77
minhduc <minhducducminh1999@gmail.com>
2019-08-10 20:05:54 +0700

    new project

--- todolist/index.js
+++ todolist/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.engine("handlebars",exphbs({defaultLayout:"main"}));
 app.set("view engine","handlebars");
 
+app.use(express.static(path.join(__dirname,'public')));
+
 app.get("/",(req,res) => {
-  res.end();
+  res.render("todolist");
 });
 
 app.listen(PORT,() => {
