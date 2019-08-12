de5ee376f88d0d9696a333dcf1f6289797e027a7
minhduc <minhducducminh1999@gmail.com>
2019-08-12 16:47:50 +0700

    create socket

--- todolist/index.js
+++ todolist/index.js
@@ -2,17 +2,34 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const path = require("path");
 const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+// dòng trên có nghĩa là đỡ mắc công tạo thêm 1 const như là const IO = io(http);
 const PORT = process.env.PORT || 8080;
 
 app.engine("handlebars",exphbs({defaultLayout:"main"}));
 app.set("view engine","handlebars");
 
 app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,"node_modules","socket.io-client","dist")));
+// dùng npm install --save socket.io nó tải về nguyên cái bộ socket cho mình gồm cho server và client
+// app.use này là để dùng cái bản io client
 
 app.get("/",(req,res) => {
   res.render("todolist");
 });
 
-app.listen(PORT,() => {
+io.on("connection",socket => {
+  // io là đường truyền input outpunt
+  // socket này có thể được coi là 1 cái modem kết nối mọi cái đường truyền lại với nhau
+  console.log("user is connected");
+  // mỗi lần có 1 user nào đó kết nối tới server thành công qua socket thì nó sẽ báo trong cái console
+  socket.on("disconnect",() => {
+    console.log("disconnected");
+  });
+  // nếu như user disconnect thì socket sẽ làm 1 thông báo
+});
+
+http.listen(PORT,() => {
   console.log(`Server is running on ${PORT}`);
 });
