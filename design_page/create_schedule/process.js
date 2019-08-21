c6b44aaf9859284950ea04b95100b382a37570e3
MinhDuc <minhducducminh1999@gmail.com>
2019-08-21 18:17:23 +0700

    done

--- design_page/create_schedule/process.js
+++ design_page/create_schedule/process.js
@@ -13,14 +13,13 @@ window.onload = () => {
     });
   });
   document.getElementById("capture").addEventListener("click",function(){
-    let link = this;
+    let link = document.createElement("a");
     html2canvas(document.querySelector("table")).then(canvas => {
       // Capture
-      document.body.appendChild(canvas)
-      let image = canvas.toDataURL("image/png");
-      console.log(image);
-      
-      // link.setAttribute("href",`${image}`);
+      let image = canvas.toDataURL("image/jpg"); // convert canvas thành Data cho URL
+      link.download = "mySchedule.jpg"; // Khi download nó sẽ auto để cái tên theo string này
+      link.href = image; // dữ liệu để download 
+      link.click(); // tự động ấn vào cái <a> đó, mặc dù mình ko add nó vào html
     });
   });
 };
