85133e956b970bd224a0d5947af2f3b565acb467
minhduc <minhducducminh1999@gmail.com>
2019-08-22 22:30:11 +0700

    dong project

---
+++ BanComProject/animation.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+  const calc = document.getElementById("calc");
+  const glass = document.getElementById("glass");
+  const board = document.getElementById("board");
+  calc.addEventListener("click",() => {
+    glass.style.display = "block";
+    board.style.right = "0";
+  });
+  glass.addEventListener("click",function(){
+    this.style.display = "none";
+    board.style.right = "-30%";
+  });
+
+}
