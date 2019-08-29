ef70f9a900dcf4f660bc0c5a00f6ad705fd4c632
minhduc <minhducducminh1999@gmail.com>
2019-08-29 17:32:12 +0700

    done

--- design_page/create_schedule/process.js
+++ design_page/create_schedule/process.js
@@ -1,10 +1,28 @@
 window.onload = () => {
+  let id = "study";
+  let work = "Đi học";
+  document.getElementById("btn-busy").addEventListener("click",function(){
+    if (this.hasAttribute("off")){
+      this.removeAttribute("off");
+      this.setAttribute("style","color: white; background:red");
+      this.innerHTML = "Busy Mode: ON";
+      id = "busy";
+      work = "Bận";
+    }
+    else{
+      this.setAttribute("off","");
+      this.removeAttribute("style");
+      this.innerHTML = "Busy Mode: OFF";
+      id = "study";
+      work = "Đi học";
+    }
+  })
   document.querySelectorAll("td#free").forEach(item => {
     item.innerHTML = "Rãnh";
     item.addEventListener("click",() => {
       if (item.id === "free"){
-        item.setAttribute("id","busy");
-        item.innerHTML = "Đi học";
+        item.setAttribute("id",id);
+        item.innerHTML = work;
       }
       else{
         item.setAttribute("id","free");
