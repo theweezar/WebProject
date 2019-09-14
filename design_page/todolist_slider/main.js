85d3aa213fd188da268a9c03c44561644d0efa89
MinhDuc <minhducducminh1999@gmail.com>
2019-09-14 22:51:29 +0700

    committed

--- design_page/todolist_slider/main.js
+++ design_page/todolist_slider/main.js
@@ -1,20 +1,20 @@
 function HideAndShowInformationTabs(){
-  $("#trigger").on("click",function(){
-    $("#profile-btn,#logout-btn").toggle(0);
-    if ($(".your-infor ul").attr("style") == undefined){
-      $(".your-infor ul").css({border:"1px solid gray"});
-      $(".arrow-down").css({transform:"rotate(-135deg)"});
-    }
-    else{
-      $(".your-infor ul").removeAttr("style");
-      $(".arrow-down").css({transform:"translateY(-3px) rotate(45deg)"});
-    }
-  })
+  // $("#trigger").on("click",function(){
+  //   $("#profile-btn,#logout-btn").toggle(0);
+  //   if ($(".your-infor ul").attr("style") == undefined){
+  //     $(".your-infor ul").css({border:"1px solid gray"});
+  //     $(".arrow-down").css({transform:"rotate(-135deg)"});
+  //   }
+  //   else{
+  //     $(".your-infor ul").removeAttr("style");
+  //     $(".arrow-down").css({transform:"translateY(-3px) rotate(45deg)"});
+  //   }
+  // })
 }
 
 $(function(){
-  $("#help").click(function(){
-    $(".help").css({top:"0"});
-  });
-  HideAndShowInformationTabs();
+  // $("#help").click(function(){
+  //   $(".help").css({top:"0"});
+  // });
+  // HideAndShowInformationTabs();
 });
