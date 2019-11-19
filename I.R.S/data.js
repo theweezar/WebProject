b55ec85ddcb0534009592db17280065de678d7b9
minhduc <minhducducminh1999@gmail.com>
2019-11-19 22:53:17 +0700

    committed

--- I.R.S/data.js
+++ I.R.S/data.js
@@ -3,6 +3,31 @@ const data = [
     "tag":"greeting",
     "input":["hi","hello","hi there","hello there","what's up","hey"],
     "output":["hi","hello my friend","hello","hiiiiiiii","from the love of God, helly my fellow human"]
+  },
+  {
+    "tag":"",
+    "input":[],
+    "output":[]
+  },
+  {
+    "tag":"",
+    "input":[],
+    "output":[]
+  },
+  {
+    "tag":"",
+    "input":[],
+    "output":[]
+  },
+  {
+    "tag":"",
+    "input":[],
+    "output":[]
+  },
+  {
+    "tag":"",
+    "input":[],
+    "output":[]
   }
 ];
 
