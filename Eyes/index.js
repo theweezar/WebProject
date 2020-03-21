2f48d3546dd8cd98439390871ebfb41c6d5b8bf8
duc <minhducducminh1999@gmail.com>
2020-03-21 22:34:20 +0700

    committed

--- Eyes/index.js
+++ Eyes/index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const fs = require("fs");
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const PORT = process.env.PORT | 8080;
+
+app.get("/",(req,res) => {
+  var p = new Promise((rs,rj) => {
+    fs.readdir("C:\\Users\\hpmdu\\Music",(err,data) => {
+      if (err) rj(err);
+      else rs(data);
+    });
+  })
+  .then(rs => {
+    res.end(`${rs}`);
+  })
+  .catch(err => {throw err;})
+})
 
 io.on("connection",socket => {
   console.log("Connection is established !");
